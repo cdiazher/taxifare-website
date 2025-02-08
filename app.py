f8c2bdb4fe0bbc0a70d66a363fcc881c3d14dbd9
Carmen Diaz Hernando <cdiazher96@gmail.com>
2025-02-08 14:50:33 -0600

    My first website

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ import requests
 import pandas as pd
 import folium
 from streamlit_folium import folium_static
+from datetime import datetime
 
 st.markdown("# Calculate your Taxi fare")
 
 col1, col2, col3, col4, col5 = st.columns([1,2,2,1,1])
 with col1:
-    date_time = st.date_input("Pick-up Date", value="2025-02-08")
+    date = st.date_input("Pick-up Date", value="2025-02-08")
+    time = st.time_input("Pick-up Time", value="12:00:00")
+date_time = datetime.combine(date, time)
 with col2:
     pickup_longitude = st.number_input("Pick-up Longitude", value=-73.935242)
     pickup_latitude = st.number_input("Pick-up Latitude", value=40.730610)
